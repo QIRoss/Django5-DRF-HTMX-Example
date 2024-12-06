ec3e390db4f703ed3d26ebe1ede410a1cf882ed6
qiross <lucasqreis@poli.ufrj.br>
2024-12-06 19:08:52 -0300

    htmx is fast

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('normal-view/', views.normal_view, name='normal_view'),
     path('api/drf-endpoint/', views.drf_endpoint, name='drf_endpoint'),
+    path('example/', views.example, name='example'),
+    path('sample-post/', views.sample_post, name='sample-post'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http import HttpResponse
+from myapp.forms import SampleForm 
 
 def normal_view(request):
     context = {'message': 'Hello from the normal view!'}
@@ -9,3 +11,16 @@ def normal_view(request):
 @api_view(['GET'])
 def drf_endpoint(request):
     return Response({'message': 'Hello from the DRF endpoint!'})
+
+def sample_post(request, *args, **kwargs):  
+    form = SampleForm(request.POST or None)  
+
+    if form.is_valid():  
+        print(f'{ form.cleaned_data= }')  
+        return HttpResponse('<p class="success">Form submitted successfully! ✅</p>')  
+    else:  
+        return HttpResponse(f'<p class="error">Your form submission was unsuccessful ❌. Please would you correct the errors? The current errors: {form.errors}</p>')
+
+
+def example(request):  
+    return render(request, 'example.html')
